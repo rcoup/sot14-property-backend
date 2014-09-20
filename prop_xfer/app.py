20a45204f0a6dca8b1674f8c9e27547cd87e5ac0
Niemand <yurii.akkermanov@gmail.com>
2014-09-20 15:11:40 +1200

    Added stats method to the backend. Fixed 'try' links.

--- prop_xfer/app.py
+++ prop_xfer/app.py
@@ -18,8 +18,60 @@ Compress(app)
 
 @app.route('/')
 def hello():
-    return 'Try <a href="/week/2013-01-05">5-12 Jan 2013</a> or <a href="/week/2013-01-05/173.8,-37.4,176.0,-35.6">5-12 Jan 2013 for Auckland</a>'
+    return 'Try <a href="/week/2013-08-03">5-12 Jan 2013</a> or <a href="/week/2013-08-03/173.8,-37.4,176.0,-35.6">5-12 Jan 2013 for Auckland</a>' \
+           + '<hr>Or stats <a href="/stats/2013-08-03">5-12 Jan 2013</a> or <a href="/stats/2013-08-03/173.8,-37.4,176.0,-35.6">5-12 Jan 2013 for Auckland</a>'
 
+@app.route('/stats/<date>', defaults={'bounds': None})
+@app.route('/stats/<date>/<bounds>')
+def stats(date, bounds):
+    """
+    Query Transfer data for a week, optionally spatially filtered.
+    Returns a GeoJSON FeatureCollection.
+    """
+    try:
+        # week should be in ISO YYYY-MM-DD format
+        week_start = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+    except ValueError as e:
+        r = jsonify({"error": str(e)})
+        r.status_code = 400
+        return r
+
+    if bounds:
+        # Optionally, filter the results spatially
+        # west,south,east,north in degrees (latitude/longitude)
+        try:
+            m = re.match(r'((-?\d+(?:\.\d+)?),){3}(-?\d+(\.\d+)?)$', bounds)
+            if not m:
+                raise ValueError("Bounds should be longitudes/latitudes in west,south,east,north order")
+            w,s,e,n = map(float, bounds.split(','))
+            if w < -180 or w > 180 or e < -180 or e > 180:
+                raise ValueError("Bounds should be longitudes/latitudes in west,south,east,north order")
+            elif s < -90 or s > 90 or n < -90 or n > 90 or s > n:
+                raise ValueError("Bounds should be longitudes/latitudes in west,south,east,north order")
+
+            if e < w:
+                bounds = MultiPolygon([box(w, s, 180, n), box(-180, s, e, n)])
+            else:
+                bounds = MultiPolygon([box(w, s, e, n)])
+        except ValueError as e:
+            r = jsonify({"error": str(e)})
+            r.status_code = 400
+            return r
+
+    # Filter the transfers - the DB query happens here
+    query = Transfer.query.filter_by(week_start=week_start)
+    if bounds:
+        query = query.filter(Transfer.location.ST_Intersects(from_shape(bounds, 4326)))
+
+    features = []
+    for transfer in query:
+        features.append(transfer.as_geojson())
+
+    # Format the response as a GeoJSON FeatureCollection
+    return jsonify({
+        "date": date,
+        "amount": len(features),
+    })
 
 @app.route('/week/<date>', defaults={'bounds': None})
 @app.route('/week/<date>/<bounds>')
